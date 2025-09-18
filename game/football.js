fbcb4d2528b542fb9ddd1144defddba5271a0451
Agu Michael <engrcharles33@ymail.com>
2025-09-18 18:03:53 +0100

    added to football file

--- game/football.js
+++ game/football.js
@@ -6,7 +6,7 @@ function goodPitch1(){
     let location = "Abuja-Stadium";
     let goodPitch1 = location;
     if(goodPitch1 !== location){
-        return`${location} is good for football`
+        return`${location} is good for football let's check it out`
     }else{
         return`${location} has a poor pitch`
     }
